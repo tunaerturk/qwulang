8ef8f63e1f003a329f6d35e5030a4c6ff351a9b1
theqwu <73068443+qwuelz@users.noreply.github.com>
2021-09-18 10:58:35 +0300

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,14 @@
+import qwu
+
+while True:
+	text = input('qwu>> ')
+	if text.strip() == "": continue
+	result, error = qwu.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
